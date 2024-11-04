f26b2cded0a05dd822411aedbc77c903db0e6b1e
velobosv1977 <velobosv33@gmail.com>
2024-11-04 18:01:43 +0300

    Initial commit 2

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("ДЗ2-2 типы переменных");
+        System.out.println("Задача 1. Объяление перменных");
+        int x=0;
+        byte y=0;
+        short z=0;
+        long k=0L;
+        float t=0f;
+        double p=0;
+        System.out.println("Задача 2. Инициализация переменных по их типу");
+        k=987678965549L;
+        }
+    }
